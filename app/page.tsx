9e5eee117f3d39e170fa9a60fc6c94820afe887a
labibkamran <lkamran.bscs23seecs@seecs.edu.pk>
2026-03-08 22:18:08 +0500

    remove any usage from home product mapping

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { products } from "@/utils/products";
 import Container from "./components/container";
 import HomeBanner from "./components/HomeBanner";
-import { truncateText } from "@/utils/truncateText";
 import ProductCard from "./components/products/ProductCard";
 
 
@@ -14,7 +13,7 @@ export default function Home() {
           <HomeBanner />
         </div>
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {products.map((product: any) => {
+          {products.map((product) => {
             return (
               <ProductCard key={product.id} data={product} />
             );
